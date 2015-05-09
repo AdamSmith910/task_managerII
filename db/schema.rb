cab3d3f52f21dd274977d06013e39559a0a553f5
Adam Smith <adamsmith910@aol.com>
2015-05-09 02:51:26 -0600

    wrote first task test and created task model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509023600) do
+ActiveRecord::Schema.define(version: 20150509085100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20150509023600) do
     t.boolean  "archived",   default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "status"
+    t.datetime "due_date"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
---
+++ spec/features/tasks/list_tasks_index_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe "the list tasks index page" do
+  before :each do
+    @bad_task = Task.create(title: "Bad Task",
+                        description: "A stupid task",
+                        status: "incomplete",
+                        due_date: 01-01-2018)
+    @good_task = Task.create(title: "Bad Task",
+                        description: "A more stupid task",
+                        status: "complete",
+                        due_date: 01-01-2018)
+
+    @bad_list = List.create(title: "Bad List")
+    @good_list = List.create(tite: "Good List")
+    
+    @bad_list.tasks << @bad_task
+    @bad_list.tasks << @good_task
+
+    @good_list.tasks << @good_task
+    @good_task.tasks << @bad_task
+  end
+
+  it "displays a list of all of the incomplete
+  tasks belonging to a list" do
+    visit "/"
+    click_link_or_button(@bad_list.title)
+
+    expect(page).to have_content(@bad_task.title)
+    expect(page).to have_content(@bad_task.description)
+    expect(page).to have_content(@bad_task.due_date)
+  end
+end
