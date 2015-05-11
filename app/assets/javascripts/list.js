bc500447dc2e2f2ed9f8b694a7a921f084c9cd0a
Adam Smith <adamsmith910@aol.com>
2015-05-11 03:52:43 -0600

    tried and failed to get javascript functionality working in production

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -26,6 +26,30 @@ var markTaskIncomplete = function(listId, taskId) {
   });
 };
 
+var markListArchived = function(listId) {
+  $.ajax('/lists/' + listId + '.json', {
+    method: 'PUT',
+    success: hideList(listId),
+    data: {
+      list: {
+        archived: true
+      }
+    }
+  });
+};
+
+var markListUnarchived = function(listId) {
+  $.ajax('/lists/' + listId + '.json', {
+    method: 'PUT',
+    success: hideList(listId),
+    data: {
+      list: {
+        archived: true
+      }
+    }
+  });
+};
+
 var hideTask = function(taskId) {
   $('#task-row-' + taskId).hide();
 };
